f8e1dd7cf8c09ace0ce2b1c2ad8a6c6b7221476c
alvaro <a.sevillanodelafuente@gmail.com>
2017-12-14 22:38:43 +0100

    sacar a rubén

--- reserva_3.sh
+++ reserva_3.sh
@@ -1,6 +1,6 @@
 sudo apt-get --yes --force-yes install firefox=45.0.2+build1-0ubuntu1
 cd /home/ubuntu/Browod
 sleep 15
-nohup python main.py ruben >ruben.log 2>&1 &
+# nohup python main.py ruben >ruben.log 2>&1 &
 sleep 5
 nohup python main.py chema >chema.log 2>&1 &
